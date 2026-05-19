f6c2287bcd651e950a430fc6e6e4f832e5cc5f89
Noah Menezes <2006noahmenezes@gmail.com>
2026-05-19 19:09:59 +0530

    style: remove vertical border lines from landing page layout

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -16,8 +16,7 @@ export function LandingPage() {
           src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260508_064122_c4750c0e-7476-4b44-94a2-a85a65c63bf2.mp4" />
       </div>
 
-      <div className="hidden md:block pointer-events-none fixed inset-y-0 left-1/2 -translate-x-[calc(50%+36rem)] w-px bg-white/10 z-[5]" />
-      <div className="hidden md:block pointer-events-none fixed inset-y-0 left-1/2 translate-x-[calc(-50%+36rem)] w-px bg-white/10 z-[5]" />
+
 
       {/* Root SVG Noise Filter */}
       <svg className="absolute w-0 h-0 pointer-events-none">
